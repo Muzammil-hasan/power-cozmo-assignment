f39816b10c883a99c75af3c6287b78cc9d3f781f
Muzammil-hasan <muzammilsyed270300@gmail.com>
2022-10-10 22:56:05 +0530

    complete: login functionality

--- layout/Index.tsx
+++ layout/Index.tsx
@@ -1,12 +1,17 @@
-import { Fragment, ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactNode } from 'react';
+import { Toaster } from 'react-hot-toast';
 import Header from './Header';
+
+const queryClient = new QueryClient();
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <Fragment>
+    <QueryClientProvider client={queryClient}>
       <Header />
       {/* <Sidebar /> */}
       <main>{children}</main>
-    </Fragment>
+      <Toaster position="top-right" />
+    </QueryClientProvider>
   );
 }
--- pages/account/register.tsx
+++ pages/account/register.tsx
@@ -1,13 +1,19 @@
 import FormWrapper from '@/components/FormWrapper';
+import LoadingIcon from '@/components/icons/LoadingIcon';
+import { axiosClient } from '@/config/api';
 import { RegistrationSchema } from '@/schemas/account.schema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { TextField } from '@mui/material';
 import Button from '@mui/material/Button';
 import { Box } from '@mui/system';
+import { useMutation } from '@tanstack/react-query';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
+import { onError, onSuccess } from 'utils';
 
-type LoginInputs = {
+type RegisterInputs = {
   name: string;
   email: string;
   phone: number;
@@ -16,29 +22,35 @@ type LoginInputs = {
 };
 
 export default function Register() {
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm<LoginInputs>({
-    resolver: zodResolver(RegistrationSchema),
-  });
+  const router = useRouter();
+  const form = useForm<RegisterInputs>({ resolver: zodResolver(RegistrationSchema) });
+  const { errors } = form.formState;
 
-  const onSubmit = (data: any) => console.log(data);
+  const { mutate, isLoading, status, data } = useMutation((data: RegisterInputs) =>
+    axiosClient.post('/register', data)
+  );
+
+  useEffect(() => {
+    if (status === 'success') {
+      onSuccess(data.data.message);
+      form.reset();
+      router.push('/account/login');
+    } else if (status === 'error') onError();
+  }, [status]);
 
   return (
     <div className="flex items-center justify-center min-h-screen">
       <FormWrapper>
         <Box
-          onSubmit={handleSubmit(onSubmit)}
           component={'form'}
           className="flex flex-col items-stretch space-y-5"
+          onSubmit={form.handleSubmit((data: RegisterInputs) => mutate(data))}
         >
           <TextField
             fullWidth
             label="Full name"
             variant="outlined"
-            {...register('name')}
+            {...form.register('name')}
             error={errors.name?.message ? true : false}
             helperText={errors.name?.message}
           />
@@ -47,7 +59,7 @@ export default function Register() {
             type={'email'}
             label="Email address"
             variant="outlined"
-            {...register('email')}
+            {...form.register('email')}
             error={errors.email?.message ? true : false}
             helperText={errors.email?.message}
           />
@@ -56,7 +68,7 @@ export default function Register() {
             type={'tel'}
             label="Phone number"
             variant="outlined"
-            {...register('phone')}
+            {...form.register('phone')}
             error={errors.phone?.message ? true : false}
             helperText={errors.phone?.message}
           />
@@ -65,7 +77,7 @@ export default function Register() {
             label="Password"
             type={'password'}
             variant="outlined"
-            {...register('password')}
+            {...form.register('password')}
             error={errors.password?.message ? true : false}
             helperText={errors.password?.message}
           />
@@ -74,18 +86,18 @@ export default function Register() {
             type={'password'}
             variant="outlined"
             label="Confirm Password"
-            {...register('password_confirmation')}
+            {...form.register('password_confirmation')}
             error={errors.password_confirmation?.message ? true : false}
             helperText={errors.password_confirmation?.message}
           />
-          <Button variant="contained" className="bg-blue-700" type="submit">
-            Register
+          <Button variant="contained" className="bg-blue-700" type="submit" disabled={isLoading}>
+            {isLoading ? <LoadingIcon className="w-6 h-6 text-white" /> : 'Register'}
           </Button>
 
           <p className="text-sm text-center">
-            Already have an account?{' '}
+            Already have an account?
             <Link href={'/account/login'}>
-              <a className="font-medium text-blue-900 hover:underline">Login here</a>
+              <a className="font-medium text-blue-900 hover:underline"> Login here</a>
             </Link>
           </p>
         </Box>
