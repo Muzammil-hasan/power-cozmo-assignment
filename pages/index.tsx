b1140a7585a23ac26082fd02b1f35f6c97303668
Muzammil-hasan <muzammilsyed270300@gmail.com>
2022-10-11 12:55:49 +0530

    refactor: home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,3 @@
-import toast from 'react-hot-toast';
-
 export default function Home() {
-  const notify = () => toast.success('Here is your toast.');
-  return (
-    <div className="grid min-h-screen place-content-center">
-      <button onClick={notify}>Toast</button>
-    </div>
-  );
+  return <div className="grid min-h-screen place-content-center">Home </div>;
 }
